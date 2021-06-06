9e137f8f872820cf6461223f87dd7f48e58294de
Squeekoo <edczjr@gmail.com>
2021-06-05 22:00:14 -0700

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const User = require("../models/user");
+const { route } = require("../app");
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+router.post('/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        if (await User.authenticate(username, password)) {
+            let token = jwt.sign({ username }, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({ token });
+        }
+        throw new ExpressError("Invalid username/password", 400);
+    } catch (e) {
+        return next(e);
+    }
+});
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
+route.post('/register', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        let token = jwt.sign({ username }, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({ token });
+    } catch (e) {
+        return next(e);
+    }
+});
+
+module.exports = router;
